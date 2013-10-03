548e5c81f8701b5cd5bd9b366181c35a2fc0b2df
as <c15as@dalton.org>
2013-10-03 11:59:52 -0400

    initial stack file, also gitignore

---
+++ IntStack.java
@@ -0,0 +1,35 @@
+public class IntStack {
+    int[] stack;
+    int top;
+
+    public IntStack(int size) {
+	top=0;
+	stack = new int[size];//make size that your stack can be--takes in param
+
+    boolean isEmpty() {
+	return top==0;
+    }
+
+    void push(int num) {
+	stack[top++]=num;
+    }
+
+    void pop() {
+	return stack[--top];//
+    }
+
+    void peek() {
+	return stack[top-1];//show the top spot
+    }
+   
+    public static void main(String[] args) {
+	IntStack is = new IntStack(10);
+	is.push(4);
+	is.push(5);
+	is.push(6);
+	int output = is.pop();
+	System.out.println(output);
+    }
+
+
+ }
