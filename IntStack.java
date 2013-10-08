87f2e4ab4c1cf481310813f1e6ab0810573184fa
anna h <c15ah@dalton.org>
2013-10-08 12:38:01 -0400

    popall

--- IntStack.java
+++ IntStack.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class IntStack {
     int[] stack;
     int top;
@@ -19,10 +21,27 @@ public class IntStack {
 	return stack[--top];//
     }
 
+    //Author: Anna Hirschorn
+    // this method pops all the ints in the array
+     int[] popAll(){
+	if(isEmpty()) return new int[] {};
+	return combine(new int[]{pop()}, popAll());
+    }
+    
+    //combine method used in popall
+    /*from javarevisted online*/
+    public static int[] combine(int[] a, int[] b){
+        int length = a.length + b.length;
+        int[] result = new int[length];
+        System.arraycopy(a, 0, result, 0, a.length);
+        System.arraycopy(b, 0, result, a.length, b.length);
+        return result;
+    }
+
     int peek() {
 	return stack[top-1];//show the top spot
     }
-   
+  
     public static void main(String[] args) {
 	IntStack is = new IntStack(10);
 	is.push(4);
@@ -30,7 +49,12 @@ public class IntStack {
 	is.push(6);
 	int output = is.pop();
 	System.out.println(output);
+
+	//tests for popall:
+	is.push(3);
+	is.push(2);
+	is.push(1);
+	int[] outputall = is.popAll();
+	System.out.println(Arrays.toString(outputall));
     }
-
-
  }
